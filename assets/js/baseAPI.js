743292143506f12167e95b7758a058a9821230f4
smin163 <s_min90@163.com>
2023-04-16 23:16:03 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,22 @@ $.ajaxPrefilter(function (options) {
     // 在发起真正的Ajax请求之前，统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url
     // console.log(options.url)
+
+    // 统一为有权限的借口设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载 complete回调函数
+    options.complete = function (res) {
+        // 在complete回调函数中，可以使用res.responseJSON中拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1.强制清空token
+            localStorage.removeItem('token')
+            // 2.强制跳转到登录页面
+            location.href = '/大事件项目/day1/code/login.html'
+        }
+    }
 })
